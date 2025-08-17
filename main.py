c3c7e7fe0463312381287ad6ad040efa79274960
Vimal Kumar Singh <85746210+visu708@users.noreply.github.com>
2025-08-17 17:42:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py
+
+from Ui.gui import EngineSimulatorGUI
+
+def main():
+    """Main function to run the GUI-based engine simulator."""
+    app = EngineSimulatorGUI()
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
